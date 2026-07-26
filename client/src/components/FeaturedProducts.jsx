5189446956255f4d675e84b159b5a0bf1bc0753f
r68720282-afk <r68720282@gmail.com>
2026-07-27 00:20:42 +0530

    Update import path for ProductCard component

--- client/src/components/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-import ProductCard from "./ProductCard";
+import ProductCard from "./product/ProductCard";
 
 const API =
   import.meta.env.VITE_API_URL
