891f7e90f3c9394505b50ea23fde49ffc3e44200
r68720282-afk <r68720282@gmail.com>
2026-07-26 23:03:49 +0530

    Replace home route with HomePage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home OK</h1>} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
