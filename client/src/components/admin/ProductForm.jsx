b72a33b553495a74527efec71755e2d7d93ab14d
r68720282-afk <r68720282@gmail.com>
2026-07-26 17:13:49 +0530

    Add ProductForm component for adding products

---
+++ client/src/components/admin/ProductForm.jsx
@@ -0,0 +1,17 @@
+export default function ProductForm() {
+
+  return (
+
+    <div className="productForm">
+
+      <h2>Add Product</h2>
+
+      <p>
+        Product Form will be connected in the next step.
+      </p>
+
+    </div>
+
+  );
+
+}
