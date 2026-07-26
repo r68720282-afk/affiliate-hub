eb754aad4e86d389a4a2c78c091ccf538e653d52
r68720282-afk <r68720282@gmail.com>
2026-07-26 22:51:01 +0530

    Update ProductTable.jsx

--- client/src/components/admin/ProductTable.jsx
+++ client/src/components/admin/ProductTable.jsx
@@ -295,41 +295,29 @@ export default function ProductTable({
 
             </table>
 
-            <div className="pagination">
+                       <div className="pagination">
+              <button
+                type="button"
+                disabled={page === 1}
+                onClick={() => setPage(page - 1)}
+              >
+                Previous
+              </button>
 
-        <button
-          type="button"
-          disabled={page === 1}
-          onClick={() =>
-            setPage(page - 1)
-          }
-        >
-          Previous
-        </button>
+              <span>
+                Page {page} of {totalPages}
+              </span>
 
-        <span>
-
-          Page {page} of {totalPages}
-
-        </span>
-
-        <button
-          type="button"
-          disabled={
-            page === totalPages
-          }
-          onClick={() =>
-            setPage(page + 1)
-          }
-        >
-          Next
-        </button>
-
-                 </div>
-
+              <button
+                type="button"
+                disabled={page === totalPages}
+                onClick={() => setPage(page + 1)}
+              >
+                Next
+              </button>
+            </div>
           </>
-        )
-      }
+        )}
 
     </div>
 
