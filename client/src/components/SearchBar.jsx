15eb1b6597610e00338c68ef03fb2fb1f1f5c5a7
r68720282-afk <r68720282@gmail.com>
2026-07-26 16:29:30 +0530

    Add SearchBar component for product search

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,20 @@
+export default function SearchBar() {
+  return (
+    <section className="searchSection">
+
+      <div className="container">
+
+        <input
+          type="text"
+          placeholder="Search Products..."
+        />
+
+        <button>
+          Search
+        </button>
+
+      </div>
+
+    </section>
+  );
+}
