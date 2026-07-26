ea33bdb492f8389d02b9c034e4d801bc67528700
r68720282-afk <r68720282@gmail.com>
2026-07-26 23:01:30 +0530

    Simplify App component with basic home route
    Removed multiple routes and components from App.jsx, replacing them with a simple home message.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,77 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
-import ProductsPage from "./components/product/ProductsPage";
-import ProductDetails from "./components/product/ProductDetails";
-import SearchPage from "./components/product/SearchPage";
-import AdminLogin from "./pages/AdminLogin";
-import AdminDashboard from "./pages/AdminDashboard";
-
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-
 export default function App() {
   return (
     <BrowserRouter>
-
-      <Header />
-
       <Routes>
-
-        <Route
-          path="/"
-          element={<HomePage />}
-        />
-
-        <Route
-          path="/products"
-          element={<ProductsPage />}
-        />
-
-        <Route
-          path="/product/:slug"
-          element={<ProductDetails />}
-        />
-
-        <Route
-          path="/search"
-          element={<SearchPage />}
-        />
-
-        <Route
-          path="/admin-login"
-          element={<AdminLogin />}
-        />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="*"
-          element={
-            <div
-              style={{
-                padding: "60px",
-                textAlign: "center"
-              }}
-            >
-              <h2>404</h2>
-              <p>Page Not Found</p>
-            </div>
-          }
-        />
-
+        <Route path="/" element={<h1>Home OK</h1>} />
       </Routes>
-
-      <Footer />
-
     </BrowserRouter>
   );
 }
