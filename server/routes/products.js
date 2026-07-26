26f2d0abbf5e9514c9ac07105b778de3a96c4db7
r68720282-afk <r68720282@gmail.com>
2026-07-26 15:54:32 +0530

    Add products route with getProducts and getProductById

---
+++ server/routes/products.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getProducts, getProductById } from "../controllers/productsController.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+
+export default router;
