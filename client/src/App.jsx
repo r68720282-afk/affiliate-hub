ea544e847166201baff9c97a569b1e298e129024
r68720282-afk <r68720282@gmail.com>
2026-07-26 22:57:42 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,73 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import HomePage from "./pages/HomePage";
-import ProductsPage from "./pages/ProductsPage";
-import ProductDetails from "./pages/ProductDetails";
-import SearchPage from "./pages/SearchPage";
-import AdminLogin from "./pages/AdminLogin";
-import AdminDashboard from "./pages/AdminDashboard";
-
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-
-      <Routes>
-        <Route
-          path="/"
-          element={<HomePage />}
-        />
-
-        <Route
-          path="/products"
-          element={<ProductsPage />}
-        />
-
-        <Route
-          path="/product/:slug"
-          element={<ProductDetails />}
-        />
-
-        <Route
-          path="/search"
-          element={<SearchPage />}
-        />
-
-        <Route
-          path="/admin-login"
-          element={<AdminLogin />}
-        />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="*"
-          element={
-            <div
-              style={{
-                padding: "60px",
-                textAlign: "center",
-              }}
-            >
-              <h2>404</h2>
-              <p>Page Not Found</p>
-            </div>
-          }
-        />
-      </Routes>
-
-      <Footer />
-    </BrowserRouter>
+    <div
+      style={{
+        padding: "50px",
+        fontSize: "32px",
+        textAlign: "center",
+      }}
+    >
+      ✅ Affiliate Hub Working
+    </div>
   );
 }
