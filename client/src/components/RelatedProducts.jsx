6db70d052d807f1305bc5b7a8e648cbb2faf8632
r68720282-afk <r68720282@gmail.com>
2026-07-26 16:32:52 +0530

    Add RelatedProducts component placeholder

---
+++ client/src/components/RelatedProducts.jsx
@@ -0,0 +1,19 @@
+export default function RelatedProducts() {
+  return (
+
+    <section className="relatedProducts">
+
+      <div className="container">
+
+        <h2>Related Products</h2>
+
+        <p>
+          Related products section will be added in the next update.
+        </p>
+
+      </div>
+
+    </section>
+
+  );
+}
