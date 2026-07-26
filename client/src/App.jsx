ea840a94872372833081a796119e6271216bdb5b
r68720282-afk <r68720282@gmail.com>
2026-07-26 17:00:40 +0530

    Add protected route for admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,37 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("adminToken");
+
+  if (!token) {
+    return <Navigate to="/admin/login" replace />;
+  }
+
+  return children;
+}
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
+
+      <Route path="/product/:id" element={<ProductPage />} />
+
+      <Route path="/admin/login" element={<AdminLogin />} />
 
       <Route
-        path="/"
-        element={<HomePage />}
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
       />
-
-      <Route
-        path="/product/:id"
-        element={<ProductPage />}
-      />
-
     </Routes>
   );
 }
