dca4a585b8d64e0691edae009f479a9e47954684
r68720282-afk <r68720282@gmail.com>
2026-07-26 16:27:35 +0530

    Create ProductPage.jsx

---
+++ client/src/pages/ProductPage.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+import ProductDetails from "../components/ProductDetails";
+import RelatedProducts from "../components/RelatedProducts";
+import Footer from "../components/Footer";
+
+export default function ProductPage() {
+  return (
+    <>
+      <Header />
+
+      <ProductDetails />
+
+      <RelatedProducts />
+
+      <Footer />
+    </>
+  );
+}
