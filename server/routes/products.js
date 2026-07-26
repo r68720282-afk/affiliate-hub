ca4892b646706a6009bf4239f7fb02a17d97c0e3
r68720282-afk <r68720282@gmail.com>
2026-07-26 17:07:14 +0530

    Add CRUD operations for products in routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,9 +1,23 @@
 import express from "express";
-import { getProducts, getProductById } from "../controllers/productsController.js";
+
+import {
+  getProducts,
+  getProductById,
+  addProduct,
+  editProduct,
+  removeProduct
+} from "../controllers/productsController.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
+
 router.get("/:id", getProductById);
+
+router.post("/", addProduct);
+
+router.put("/:id", editProduct);
+
+router.delete("/:id", removeProduct);
 
 export default router;
