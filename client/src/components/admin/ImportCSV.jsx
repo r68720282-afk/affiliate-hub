db571b6d519f9a4fdd4944f4db81f17043167ea7
r68720282-afk <r68720282@gmail.com>
2026-07-29 22:21:17 +0530

    Update ImportCSV.jsx

--- client/src/components/admin/ImportCSV.jsx
+++ client/src/components/admin/ImportCSV.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import CSVUploader from "./CSVUploader";
 
 export default function ImportCSV() {
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const handleImportComplete = () => {
+    setRefreshKey((prev) => prev + 1);
+  };
+
   return (
     <div className="adminPage">
 
@@ -8,12 +15,16 @@ export default function ImportCSV() {
         <h1>CSV Product Import</h1>
 
         <p>
-          Upload a CSV file to import products into your Affiliate Hub.
+          Upload a CSV file to quickly import products into your Affiliate Hub.
+          Duplicate products will be skipped automatically.
         </p>
       </div>
 
       <div className="adminCard">
-        <CSVUploader />
+        <CSVUploader
+          key={refreshKey}
+          onImportComplete={handleImportComplete}
+        />
       </div>
 
     </div>
