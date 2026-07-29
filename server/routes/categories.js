03bca82b756e5da8c964456727dd5d4ac075874e
r68720282-afk <r68720282@gmail.com>
2026-07-29 22:11:59 +0530

    Update categories.js

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -5,16 +5,40 @@ import {
   createCategory,
   updateCategory,
   deleteCategory
-} from "../controllers/categories.js";
+} from "../controllers/categoriesController.js";
 
 const router = express.Router();
 
+/*
+========================================
+GET ALL CATEGORIES
+GET /api/categories
+========================================
+*/
 router.get("/", getCategories);
 
+/*
+========================================
+ADD NEW CATEGORY
+POST /api/categories
+========================================
+*/
 router.post("/", createCategory);
 
+/*
+========================================
+UPDATE CATEGORY
+PUT /api/categories/:id
+========================================
+*/
 router.put("/:id", updateCategory);
 
+/*
+========================================
+DELETE CATEGORY
+DELETE /api/categories/:id
+========================================
+*/
 router.delete("/:id", deleteCategory);
 
 export default router;
