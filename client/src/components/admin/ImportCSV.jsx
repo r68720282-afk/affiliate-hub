76e8c727b659c1f27150ae64bf4a43e56e1cbdc4
r68720282-afk <r68720282@gmail.com>
2026-07-31 17:45:04 +0530

    Refactor handleImportComplete function and update JSX

--- client/src/components/admin/ImportCSV.jsx
+++ client/src/components/admin/ImportCSV.jsx
@@ -2,31 +2,47 @@ import { useState } from "react";
 import CSVUploader from "./CSVUploader";
 
 export default function ImportCSV() {
+
   const [refreshKey, setRefreshKey] = useState(0);
 
-  const handleImportComplete = () => {
+  function handleImportComplete() {
+
     setRefreshKey((prev) => prev + 1);
-  };
+
+  }
 
   return (
-    <div className="adminPage">
 
-      <div className="adminHeader">
-        <h1>CSV Product Import</h1>
+    <div className="csvUploader">
+
+      <div className="adminCard">
+
+        <h2>
+
+          📂 CSV Product Import
+
+        </h2>
 
         <p>
+
           Upload a CSV file to quickly import products into your Affiliate Hub.
           Duplicate products will be skipped automatically.
+
         </p>
+
       </div>
 
       <div className="adminCard">
+
         <CSVUploader
           key={refreshKey}
           onImportComplete={handleImportComplete}
         />
+
       </div>
 
     </div>
+
   );
+
 }
